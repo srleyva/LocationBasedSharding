7b4b15985da41599ac119662a8b8538eadd65049
Stephen Leyva <sleyva1297@gmail.com>
2022-01-25 00:45:45 -0800

    Change recursive approach to iterative approach
    This means we don't have to spawn a new thread (less OS SysCall Overhead)
    and we don't have to bump the stack size as this is all done in the same stack frame

--- src/cell_list.rs
+++ src/cell_list.rs
@@ -30,50 +30,44 @@ impl CellScorer for UserCountScorer {
     }
 }
 
+/// CellList is a given order map where the key is the CellID
+/// and the value is the cell score
 pub struct CellList {
     storage_level: u64,
     cell_list: BTreeMap<CellID, i32>,
 }
 
 impl CellList {
+    /// Generates a Collection of cells based off of the given storage level
     pub fn new(storage_level: u64) -> Self {
         let starting_cell_id = CellID::from(ll!(0.00000000, 0.00000000));
-        let mut cell_list = BTreeMap::new();
-        let child_tread = Builder::new()
-            .stack_size(50 * 1024 * 1024)
-            .spawn(move || {
-                Self::gather_cells(storage_level, starting_cell_id, &mut cell_list);
-                cell_list
-            })
-            .expect("could not spawn thread to build s2 cells");
-
-        let cell_list = child_tread.join().expect("err building s2 list");
-
+        let cell_list = Self::gather_cells(storage_level, starting_cell_id);
         Self {
             storage_level,
             cell_list,
         }
     }
 
+    /// returns an exclusive reference to the internal cell_list
     pub fn mut_cell_list(&mut self) -> &mut BTreeMap<CellID, i32> {
         &mut self.cell_list
     }
 
-    pub fn scored_cells(&self) -> &BTreeMap<CellID, i32> {
+    /// return a read reference to the cell_list
+    pub fn cell_list(&self) -> &BTreeMap<CellID, i32> {
         &self.cell_list
     }
 
-    fn gather_cells(storage_level: u64, cell_id: CellID, seen: &mut BTreeMap<CellID, i32>) {
-        let current_cell_neighbors = cell_id.vertex_neighbors(storage_level);
-        for neighbor in current_cell_neighbors {
-            match seen.get(&neighbor) {
-                Some(_) => (),
-                None => {
-                    seen.insert(neighbor, 0);
-                    Self::gather_cells(storage_level, neighbor, seen);
-                }
-            };
+    fn gather_cells(storage_level: u64, starting_cell_id: CellID) -> BTreeMap<CellID, i32> {
+        let mut seen = BTreeMap::new();
+        let mut current_stack = vec![starting_cell_id];
+        while let Some(current_neighbor) = current_stack.pop() {
+            if !seen.contains_key(&current_neighbor) {
+                current_stack.append(&mut current_neighbor.vertex_neighbors(storage_level));
+                seen.insert(current_neighbor, 0);
+            }
         }
+        seen
     }
 }
 
@@ -84,6 +78,6 @@ mod test {
     #[test]
     fn test_geoshard_cell_list() {
         let cell_list = CellList::new(8).cell_list;
-        assert_eq!(cell_list.len(), 393216);
+        assert_eq!(cell_list.len(), 393217);
     }
 }
